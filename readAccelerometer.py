9e00c0965f300be122fbea070c3227b495b9ee19
Brady G <bradygoenner@gmail.com>
2021-11-29 16:17:40 -0700

    Reads accelerometer, has some bugs related some calibration

--- readAccelerometer.py
+++ readAccelerometer.py
@@ -3,7 +3,7 @@
 import serial
 
 # for plotting
-import matplotlib.pyplot as plt
+import matplotlib.pyplot    as plt
 import matplotlib.animation as animation
 
 import time
@@ -12,17 +12,17 @@ from scipy.fftpack import fft, fftfreq
 import numpy as np
 
 # Create figure for plotting
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
-xs = []
-ys1 = []
-ys2 = []
-ys3 = []
+#fig = plt.figure()
+#ax = fig.add_subplot(1, 1, 1)
+#xs = []
+#ys1 = []
+#ys2 = []
+#ys3 = []
 
 # for ard code 
 skip_start = True
 
-read_time        = 6
+read_time        = 10
 calibration_time = 2
 
 def animate(i, xs, ys):
@@ -57,7 +57,6 @@ def animate(i, xs, ys):
 
 def Average(lst):
     return sum(lst) / len(lst)
-
 
 
 serialPort = serial.Serial(port = "COM3", baudrate=115200,
@@ -122,7 +121,7 @@ while(1):
                         else:
                             buff[i].append(int(data))
 
-                    #print(buff)
+                    # print(buff)
 
                 skip_start = not skip_start
 
@@ -141,7 +140,7 @@ while(1):
             Xbuff = []
             Ybuff = []
             Zbuff = []
-            buff = [tbuff, Xbuff, Ybuff, Zbuff]
+            buff  = [tbuff, Xbuff, Ybuff, Zbuff]
 
             start_time =+ time.time()
             current_time = 0
@@ -163,31 +162,84 @@ while(1):
                         else:
                             buff[i].append(int(data) - AvgA[i])
 
-                    #print(buff)
+                    # print(buff)
 
                 skip_start = not skip_start
             # plot buffers
             fig, (ax1, ax2, ax3) = plt.subplots(3)
-            fig.suptitle('Vertically stacked subplots')
+            fig.suptitle('Accelerometer Readings')
             ax1.plot(buff[0], buff[1])
             ax2.plot(buff[0], buff[2])
             ax3.plot(buff[0], buff[3])
-            plt.show()
+            #plt.show()
+
+            vel_x = []
+            vel_y = []
+            vel_z = []
+
+            pos_x = []
+            pos_y = []
+            pos_z = []
+
+
+            for i, val in enumerate(buff[0]):
+                if i > 0:
+                    vel_x.append( (buff[1][i] - buff[1][i-1]/(buff[0][i] - buff[0][i-1]) ))
+                    vel_y.append( (buff[2][i] - buff[2][i-1]/(buff[0][i] - buff[0][i-1]) ))
+                    vel_z.append( (buff[3][i] - buff[3][i-1]/(buff[0][i] - buff[0][i-1]) ))
+
+            #print(vel_x)
+
+            for i, val in enumerate(vel_x):
+                if i > 0:
+                    pos_x.append( (vel_x[i] - vel_x[i-1]/(buff[0][i] - buff[0][i-1]) ))
+                    pos_y.append( (vel_y[i] - vel_y[i-1]/(buff[0][i] - buff[0][i-1]) ))
+                    pos_z.append( (vel_z[i] - vel_z[i-1]/(buff[0][i] - buff[0][i-1]) ))
+
+            
+            fig, (ax1, ax2, ax3) = plt.subplots(3)
+            fig.suptitle('Accelerometer Readings Velocity')
+            
+            ax1.plot(buff[0][0:len(vel_x)], vel_x)
+            ax2.plot(buff[0][0:len(vel_y)], vel_y)
+            ax3.plot(buff[0][0:len(vel_z)], vel_z)
+            
+            """
+            ax1.plot(buff[0:len(vel_x)-1], vel_x)
+            ax2.plot(buff[0:len(vel_y)-1], vel_y)
+            ax3.plot(buff[0:len(vel_z)-1], vel_z)
+            """
+            
+             
+            fig, (ax1, ax2, ax3) = plt.subplots(3)
+            fig.suptitle('Accelerometer Readings Position')
+            ax1.plot(buff[0][0:len(pos_x)], pos_x)
+            ax2.plot(buff[0][0:len(pos_y)], pos_y)
+            ax3.plot(buff[0][0:len(pos_z)], pos_z)
+            
 
             ## FFT analysis
 
             N = len(buff[0])
             T = (buff[0][0] - buff[0][N-1])/N
 
-            yf = fft(buff[1])
-            xf = fftfreq(N, T)[:N//2]
+            yf1 = fft(buff[1])
+            xf1 = fftfreq(N, T)[:N//2]
 
-            plt.plot(xf, 2.0/N * np.abs(yf[0:N//2]))
+            yf2 = fft(buff[2])
+            xf2 = fftfreq(N, T)[:N//2]
+
+            yf3 = fft(buff[3])
+            xf3 = fftfreq(N, T)[:N//2]
+
+            fig, (ax1, ax2, ax3) = plt.subplots(3)
+            ax1.plot(xf1, 2.0/N * np.abs(yf1[0:N//2]))
+            ax2.plot(xf2, 2.0/N * np.abs(yf2[0:N//2]))
+            ax3.plot(xf3, 2.0/N * np.abs(yf3[0:N//2]))
             plt.grid()
             plt.show()
 
         
-
         # Tell the device connected over the serial port that we recevied the data!
         # The b at the beginning is used to indicate bytes!
         # serialPort.write(b"Thank you for sending data \r\n")
